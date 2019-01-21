c6e845461d2cbb437db285a76cff6e6bacf377e6
bmartinko <bennett.martinko@gmail.com>
2019-01-21 15:12:02 -0500

    I think this is gonna work. Hopefully...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,17 +9,19 @@ public class Main {
     System.out.println("This program will solve the puzzle for you");
     System.out.println("Please input the number of discs to solve for:");
 
-    try{
-        Scanner kbsreader = new Scanner(System.in);
-        int disc = kbsreader.nextInt();
-    }catch (InputMismatchException e){
+    Scanner kb = new Scanner(System.in);
+    int disc = kb.nextInt();
 
-        Scanner kbsreader = new Scanner(System.in);
-        int disc = kbsreader.nextInt();
+        System.out.println("Here are the steps necessary to figure complete Tower Of Hanoi with " + disc + " discs!");
+    towerOfHanoi(disc, 'A', 'B', 'C' );
     }
-
-
-    TowersOfHanoi towerBoi = new TowersOfHanoi(3);
-    towerBoi.solveThisJawn();
+    static void towerOfHanoi(int n, char start, char mid, char end){
+        if(n==1){
+            System.out.println("Move disc 1 from rod " + start + " to rod " + end);
+        }else{
+            towerOfHanoi(n-1, start, end, mid);
+            System.out.println("Move disc " + n + " from rod " + start + " to rod " + end);
+            towerOfHanoi(n-1, mid, start, end);
+        }
     }
 }
